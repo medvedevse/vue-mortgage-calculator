344ae173c3fe2bd6024cb2ca202c59da37e5a402
Stepan <stepan.medvedev1995@yandex.ru>
2025-03-04 01:39:22 +0200

    Add vercel.json

--- src/components/NotFoundComponent.vue
+++ src/components/NotFoundComponent.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="not-found-wrapper">
 		<div class="not-found">
-			<h1 class="not-found-header">Ошибка 404</h1>
+			<h1 class="not-found-header">Что то пошло не так!</h1>
 			<p class="not-found-text">Страница не найдена</p>
 			<img
 				src="https://img.lovepik.com/element/40021/7866.png_1200.png"
 				alt="Страница не найдена"
-				width="650"
-				height="400"
+				width="800"
+				height="500"
 			/>
 		</div>
 	</div>
@@ -26,5 +26,9 @@
 .not-found-header,
 .not-found-text {
 	text-align: center;
+}
+
+.not-found-text {
+	font-size: 26px;
 }
 </style>
